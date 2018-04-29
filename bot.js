909a3256f508b14b60dc1a64c9a14898a0fa535c
Adrian Kuper <adrian.kuper@hotmail.de>
2018-04-29 17:55:18 +0200

    - added sqlite3 - added register-command for registering player in sqlite db - added version command to display stat-g-bot version - addded data-model.md file thats contains db data model infos

--- bot.js
+++ bot.js
@@ -2,6 +2,11 @@ var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 var https = require('https');
+var sqlite3 = require('sqlite3').verbose();
+var fs = require('fs');
+var package = require('./package.json');
+
+const DB_FILE_NAME = 'stat-g-db.db';
 
 const PUBG_API_BASE_URL = "https://api.playbattlegrounds.com/shards/pc-eu/";
 const PUBG_API_KEY = auth.pubgApiKey;
@@ -23,6 +28,32 @@ bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
+
+    // falls keine Datenbank-Datei vorhanden ist
+    if (!fs.existsSync(DB_FILE_NAME)) {
+
+        logger.info('No database file found. Create new database...');
+        var db = new sqlite3.Database(DB_FILE_NAME);
+
+        try {
+            // Datenmodell erstellen
+            db.serialize(function () {
+                logger.info('Creating table \"registered_player\"...')
+                db.run("CREATE TABLE registered_player (id INTEGER PRIMARY KEY, discord_name TEXT, discord_id TEXT, pubg_name TEXT, pubg_id TEXT)");
+            });
+
+            logger.info('Database created!');
+
+        } catch (err) {
+
+            logger.info('Database creation failed!')
+        } finally {
+
+            db.close();
+        }
+    }
+
+    // TODO get max id of tables
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
@@ -33,6 +64,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         var cmd = args[0];
 
         args = args.splice(1);
+
+        logger.info('user: ' + user + ', userId: ' + userID + ', channelId: ' + channelID + ', evt: ' + evt)
+
         switch (cmd) {
             // !ping
             case 'ping':
@@ -51,7 +85,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
             case 'test':
 
-                const options = {
+                var options = {
                     hostname: 'api.playbattlegrounds.com',
                     path: '/shards/pc-eu/matches/b8aee556-5912-4f3d-8734-8fa297f5d544',
                     method: 'GET',
@@ -72,7 +106,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     // The whole response has been received. Print out the result.
                     resp.on('end', () => {
                         // console.log(JSON.parse(data).explanation);
-                        
+
                         var apiData = JSON.parse(data);
                         logger.info('Loaded api data');
 
@@ -88,7 +122,75 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 });
 
                 break;
-            // Just add any case commands if you want to..
+
+            case 'register':
+
+                var options = {
+                    hostname: 'api.playbattlegrounds.com',
+                    path: '/shards/pc-eu/players?filter[playerNames]=' + args,
+                    method: 'GET',
+                    headers: {
+                        'Authorization': 'Bearer ' + PUBG_API_KEY,
+                        'Accept': 'application/vnd.api+json'
+                    }
+                };
+
+                https.get(options, (resp) => {
+                    let data = '';
+
+                    // A chunk of data has been recieved.
+                    resp.on('data', (chunk) => {
+                        data += chunk;
+                    });
+
+                    // The whole response has been received. Print out the result.
+                    resp.on('end', () => {
+
+                        var pubgPlayerData = JSON.parse(data).data[0];
+                        logger.info('Loaded api data');
+
+                        var db = new sqlite3.Database(DB_FILE_NAME);
+                        try {
+                            
+                            db.serialize(function () {
+
+                                var stmt = db.prepare("INSERT INTO registered_player (discord_name, discord_id, pubg_name, pubg_id) VALUES (?, ?, ?, ?)");
+                                stmt.run(user, userID, pubgPlayerData.attributes.name, pubgPlayerData.id);
+                                stmt.finalize();
+                            });
+                            
+                            bot.sendMessage({
+                                to: channelID,
+                                message: 'Player \"' + args + '\" registered!'
+                            });
+
+                        } catch (err) {
+
+                            logger.warn(err);
+                            bot.sendMessage({
+                                to: channelID,
+                                message: 'Error on registering player: \"' + args + '\"'
+                            });
+                        } finally {
+                            db.close();
+                        }
+                    });
+
+                }).on("error", (err) => {
+                    logger.warn("Error: " + err.message);
+                    bot.sendMessage({
+                        to: channelID,
+                        message: 'Error on registering player: \"' + args + '\"'
+                    });
+                });
+
+                
+                break;
+            case 'version':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Stat-G-Bot v' + package.version
+                });
         }
     }
 });
