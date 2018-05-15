18c8be93803730c8a7ee00fe360a6e87f135c8a5
blooby <adrian.kuper@hotmail.de>
2018-05-15 20:25:53 +0200

    set up for unit tests with chai and mocha

---
+++ test/bot.test.js
@@ -0,0 +1,7 @@
+let expect = require('chai').expect;
+
+describe('test-1', () => {
+    it('some-1', () => {
+        expect(true).to.be.true;
+    })
+})
---
+++ test/modules/cache.test.js
@@ -0,0 +1,7 @@
+let expect = require('chai').expect;
+
+describe('test-2', () => {
+    it('some-2', () => {
+        expect(true).to.be.true;
+    })
+})
