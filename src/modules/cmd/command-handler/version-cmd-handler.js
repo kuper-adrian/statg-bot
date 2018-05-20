4d8993e3da29f77ffd8dfa7ef37adb4b1b035acf
DESKTOP-BVISHD1\Adrian <adrian.kuper@hotmail.de>
2018-05-20 16:03:32 +0200

    changed styling of version command bot answer

--- src/modules/cmd/command-handler/version-cmd-handler.js
+++ src/modules/cmd/command-handler/version-cmd-handler.js
@@ -1,4 +1,5 @@
 const CommandHandler = require('./cmd-handler.js').CommandHandler;
+const moment = require('moment');
 
 const version = require('../../../../package.json').version;
 const author = require('../../../../package.json').author;
@@ -17,11 +18,35 @@ class VersionCommandHandler extends CommandHandler {
     handle(cmd, bot, db, pubg) {
         bot.sendMessage({
             to: cmd.discordUser.channelId,
-            message: `statg-bot v${version} by ${author}`
+            message: `\`\`\`statg-bot v${version} by ${author}\`\`\``
         });
+
+        // bot.sendMessage({
+        //     to: cmd.discordUser.channelId,
+        //     embed: {
+        //         "color": 9101324,
+        //         "timestamp": `${moment().toString()}`,
+        //         "footer": {
+        //             "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+        //             "text": `by ${author}`
+        //         },
+        //         "fields": [
+        //             {
+        //                 "name": "Version",
+        //                 "value": `${version}`
+        //             },
+        //             {
+        //                 "name": "Source",
+        //                 "value": "[BitBucket](https://bitbucket.org/Blooby/stat-g/src/master/)"
+        //             }
+        //         ]
+        //     }
+        // })
     }
+
+
 }
 
-exports.getHandler = function() {
+exports.getHandler = function () {
     return new VersionCommandHandler();
 }
