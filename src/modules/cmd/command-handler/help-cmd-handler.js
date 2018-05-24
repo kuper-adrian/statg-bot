8b67604903dd60d4a4da42e09b32d32c3226bdd2
blooby <adrian.kuper@hotmail.de>
2018-05-24 06:36:31 +0200

    - added tests for passing arguments to help command - refactored help message

--- src/modules/cmd/command-handler/help-cmd-handler.js
+++ src/modules/cmd/command-handler/help-cmd-handler.js
@@ -1,31 +1,20 @@
 const CommandHandler = require('./cmd-handler.js').CommandHandler;
 
 const HELP_MESSAGE = `\`\`\`
-stat-g is a discord bot that can post PUBG statistics in your discord channel!
-
-# Commands
-
 * \`!statg register [pubg player name]\` 
   Links your PUBG player name to your Discord account. Enables you to fetch your stats.
-* \`!statg stats\`
-  Shows your current season stats (if you have registered yourself).
-  **arguments**
-  * \`solo\` Stats from third-person solo matches only.
-  * \`solo-fpp\` Stats from first-person solo matches only.
-  * \`duo\` Stats from third-person duo matches only.
-  * \`duo-fpp\` Stats from first-person duo matches only.
-  * \`squad\` Stats from third-person squad matches only.
-  * \`squad-fpp\` Stats from first-person squad matches only.
-* \`!statg match\`
-  Shows your and your squads latest match info (all game modes combined).
-* \`!statg help\`
-  Displays help about commands.
-* \`!statg version\`
-  Displays version of the stat-g bot.
-* \`!statg status\`
-  Displays the current status of the PUBG api.
-* \`!statg ping\`
-  Pong!\`\`\``
+* \`!statg stats\` Shows your current season stats (if you have registered yourself). **Optional arguments** are:
+    * \`solo\` Stats from third-person solo matches only.
+    * \`solo-fpp\` Stats from first-person solo matches only.
+    * \`duo\` Stats from third-person duo matches only.
+    * \`duo-fpp\` Stats from first-person duo matches only.
+    * \`squad\` Stats from third-person squad matches only.
+    * \`squad-fpp\` Stats from first-person squad matches only.
+* \`!statg match\` Shows your and your squads latest match info (all game modes combined).
+* \`!statg help\` Displays help about commands.
+* \`!statg version\` Displays version of the stat-g bot.
+* \`!statg status\` Displays the current status of the PUBG api.
+* \`!statg ping\` Pong!\`\`\``
 
 /**
  * Handler for the "help" command
@@ -38,8 +27,15 @@ class HelpCommandHandler extends CommandHandler {
 
     handle(cmd, bot, db, pubg) {
 
+        const channelId = cmd.discordUser.channelId; 
+
+        if (cmd.arguments.length !== 0) {
+            this._onError(bot, channelId, "invalid amount of arguments");
+            return;
+        }
+
         bot.sendMessage({
-            to: cmd.discordUser.channelId,
+            to: channelId,
             message: HELP_MESSAGE
         });
     }
--- test/modules/cmd/command-handler/help-command-handler.test.js
+++ test/modules/cmd/command-handler/help-command-handler.test.js
@@ -1,9 +1,40 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
 
+const logger = require('../../../../src/modules/log').getLogger();
 const HelpCommandHandler = require('../../../../src/modules/cmd/command-handler/help-cmd-handler');
 
 describe('HelpCommandHandler.handle()', () => {
+
+    let debugStub = {};
+    let infoStub = {};
+    let warnStub = {};
+    let errorStub = {};
+
+    beforeEach(() => {
+
+        // stub all log functions
+        debugStub = sinon.stub(logger, "debug").callsFake((message) => {
+            // do nothing
+        });
+        infoStub = sinon.stub(logger, "info").callsFake((message) => {
+            // do nothing
+        });
+        warnStub = sinon.stub(logger, "warn").callsFake((message) => {
+            // do nothing
+        });
+        errorStub = sinon.stub(logger, "error").callsFake((message) => {
+            // do nothing
+        });
+    });
+
+    afterEach(() => {
+        
+        debugStub.restore();
+        infoStub.restore();
+        warnStub.restore();
+        errorStub.restore();
+    });
 
     it('should send a message to the right channel', () => {
 
@@ -12,7 +43,9 @@ describe('HelpCommandHandler.handle()', () => {
         let passedChannelId = '';
         let passedMessage = '';
 
-        const cmd = {};
+        const cmd = {
+            arguments: []
+        };
         const bot = {
             sendMessage: function(params) {
                 passedChannelId = params.to;
@@ -43,7 +76,9 @@ describe('HelpCommandHandler.handle()', () => {
         let passedChannelId = '';
         let passedMessage = '';
 
-        const cmd = {};
+        const cmd = {
+            arguments: []
+        };
         const bot = {
             sendMessage: function(params) {
                 passedChannelId = params.to;
@@ -71,5 +106,77 @@ describe('HelpCommandHandler.handle()', () => {
         expect(passedMessage).to.contain('!statg stats');
         expect(passedMessage).to.contain('!statg status');
         expect(passedMessage).to.contain('!statg version');
-    })
+    });
+
+    it('should send an error if there is a single argument given', () => {
+
+        const handler = HelpCommandHandler.getHandler();
+        
+        let passedChannelId = '';
+        let passedMessage = '';
+
+        const cmd = {
+            arguments: [
+                "some-arg"
+            ]
+        };
+        const bot = {
+            sendMessage: function(params) {
+                passedChannelId = params.to;
+                passedMessage = params.message;
+            }
+        };
+        const db = {};
+        const pubg = {};
+
+        let sendMessageSpy = sinon.spy(bot, 'sendMessage');
+
+        cmd.discordUser = {};
+        cmd.discordUser.channelId = '123';
+
+        //cmd, bot, db, pubg
+        handler.handle(cmd, bot, db, pubg);
+        sendMessageSpy.restore();
+
+        sinon.assert.calledOnce(sendMessageSpy);
+        expect(passedChannelId).to.be.equal('123');
+        expect(passedMessage).to.contain("invalid amount of arguments")
+    });
+
+    it('should send an error if there are multiple arguments given', () => {
+
+        const handler = HelpCommandHandler.getHandler();
+        
+        let passedChannelId = '';
+        let passedMessage = '';
+
+        const cmd = {
+            arguments: [
+                "some-arg",
+                "some-other-arg",
+                "some-final-arg"
+            ]
+        };
+        const bot = {
+            sendMessage: function(params) {
+                passedChannelId = params.to;
+                passedMessage = params.message;
+            }
+        };
+        const db = {};
+        const pubg = {};
+
+        let sendMessageSpy = sinon.spy(bot, 'sendMessage');
+
+        cmd.discordUser = {};
+        cmd.discordUser.channelId = '123';
+
+        //cmd, bot, db, pubg
+        handler.handle(cmd, bot, db, pubg);
+        sendMessageSpy.restore();
+
+        sinon.assert.calledOnce(sendMessageSpy);
+        expect(passedChannelId).to.be.equal('123');
+        expect(passedMessage).to.contain("invalid amount of arguments")
+    });
 })
