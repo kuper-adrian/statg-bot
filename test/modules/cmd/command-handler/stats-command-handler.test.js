48dc12fbc8cfdac0619cc6f9736da4ca73a95103
blooby <adrian.kuper@hotmail.de>
2018-05-28 16:53:58 +0200

    added test cases for stats cmd (TODO implementation)

--- test/modules/cmd/command-handler/stats-command-handler.test.js
+++ test/modules/cmd/command-handler/stats-command-handler.test.js
@@ -41,4 +41,56 @@ describe('StatsCommandHandler.handle()', () => {
     it('should get the pubg id for the discord user from the db', () => {
         // TODO
     });
-})
+
+    it('should throw an error if there is not player id for discord user in db', () => {
+        // TODO
+    })
+
+    it('should request the seasons from pubg api after retrieving the id from db', () => {
+        // TODO
+    })
+
+    it('should throw an error if multiple player ids are returned by db query', () => {
+        // TODO
+    })
+
+    it('should request the stats for the given player id and current season', () => {
+        // TODO
+    });
+
+    it('should return return the avg. stats for all game modes if no argument was passed to cmd', () => {
+        // TODO
+    });
+
+    it('should return avg. stats for game mode "solo" when passed as argument', () => {
+        // TODO
+    });
+
+    it('should return avg. stats for game mode "solo-fpp" when passed as argument', () => {
+        // TODO
+    });
+
+    it('should return avg. stats for game mode "duo" when passed as argument', () => {
+        // TODO
+    });
+
+    it('should return avg. stats for game mode "duo-fpp" when passed as argument', () => {
+        // TODO
+    });
+
+    it('should return avg. stats for game mode "squad" when passed as argument', () => {
+        // TODO
+    });
+
+    it('should return avg. stats for game mode "squad-fpp" when passed as argument', () => {
+        // TODO
+    });
+
+    it('should throw an error if an invalid argument was passed', () => {
+        // TODO
+    });
+
+    it('should throw an error if multiple arguments were passed', () => {
+        // TODO
+    });
+});
