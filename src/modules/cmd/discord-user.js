a60e0b6c7900d57f0bd550bf7e401d3c750fc7db
DESKTOP-BVISHD1\Adrian <adrian.kuper@hotmail.de>
2018-06-19 15:32:50 +0200

    removed linting errors from discord-user.js

--- src/modules/cmd/discord-user.js
+++ src/modules/cmd/discord-user.js
@@ -1,18 +1,16 @@
-
 class DiscordUser {
-
-    /**
-     * Model class to temporary store infos about a discord user.
-     * 
-     * @param {string} id Discord ID of user
-     * @param {string} name Discord name of user
-     * @param {string} channelId Id of the channel the user posted
-     */
-    constructor (id, name, channelId) {
-        this.id = id;
-        this.name = name;
-        this.channelId = channelId;
-    }
+  /**
+   * Model class to temporary store infos about a discord user.
+   *
+   * @param {string} id Discord ID of user
+   * @param {string} name Discord name of user
+   * @param {string} channelId Id of the channel the user posted
+   */
+  constructor(id, name, channelId) {
+    this.id = id;
+    this.name = name;
+    this.channelId = channelId;
+  }
 }
 
-exports.DiscordUser = DiscordUser
+exports.DiscordUser = DiscordUser;
