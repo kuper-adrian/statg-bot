d8c19f5fbb5834c779b2a77c7066083690ae9537
DESKTOP-BVISHD1\Adrian <adrian.kuper@hotmail.de>
2018-06-19 13:53:53 +0200

    removed linting errors from log.js

--- src/modules/log.js
+++ src/modules/log.js
@@ -2,25 +2,25 @@ const logger = require('winston');
 const fs = require('fs');
 
 // make sure that logs folder exists
-const logFolderPath = "./logs"
+const logFolderPath = './logs';
 if (!fs.existsSync(logFolderPath)) {
-    fs.mkdirSync(logFolderPath);
+  fs.mkdirSync(logFolderPath);
 }
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
-    colorize: true
+  colorize: true,
 });
 logger.add(logger.transports.File, {
-    filename: './logs/combined.log',
-    maxsize: '2000000', 
-    maxFiles: '5', 
-    timestamp: true,
-    json: false
+  filename: './logs/combined.log',
+  maxsize: '2000000',
+  maxFiles: '5',
+  timestamp: true,
+  json: false,
 });
 logger.level = 'debug';
 
-exports.getLogger = function () {
-    return logger;
-}
+exports.getLogger = function getLogger() {
+  return logger;
+};
