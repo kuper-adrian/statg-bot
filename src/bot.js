da5f6d69dafdae1f4663d5a58c9707efec74b0e3
DESKTOP-BVISHD1\Adrian <adrian.kuper@hotmail.de>
2018-06-19 13:11:46 +0200

    removed linting errors from bot.js

--- src/bot.js
+++ src/bot.js
@@ -9,58 +9,50 @@ const auth = require('./auth');
 let initialized = false;
 
 try {
-    process.argv.splice(0, 2);
-    auth.init(process.argv);
+  process.argv.splice(0, 2);
+  auth.init(process.argv);
 } catch (error) {
-    logger.error(error);
-    process.exit();
+  logger.error(error);
+  process.exit();
 }
-
 
 // Initialize Discord Bot
 const bot = new Discord.Client({
-    token: auth.discordToken,
-    autorun: true
+  token: auth.discordToken,
+  autorun: true,
 });
 
+bot.on('ready', () => {
+  logger.info('Connected!');
+  logger.debug('Logged in as: ');
+  logger.debug(`${bot.username} - (${bot.id})`);
 
-bot.on('ready', function (evt) {
+  statgDb.init()
+    .then(() => {
+      logger.info('start listening for messages...');
+      initialized = true;
+    })
 
-    logger.info('Connected!');
-    logger.debug('Logged in as: ');
-    logger.debug(bot.username + ' - (' + bot.id + ')');
-
-    statgDb.init()
-        .then(() => {
-            logger.info("start listening for messages...");
-            initialized = true;
-        })
-
-        .catch(error => {
-            logger.error(error.message);
-        })
+    .catch(error => logger.error(error.message));
 });
 
 bot.on('message', (user, userID, channelID, message, evt) => {
+  // dont do anything if not everything was initialized in the background
+  if (!initialized) return;
 
-    // dont do anything if not everything was initialized in the background
-    if (!initialized) return;
-
-    try {
-        cmder.processMessage(bot, statgDb, pubg, user, userID, channelID, message, evt);
-    } catch (err) {
-        logger.error(err);
-    }
+  try {
+    cmder.processMessage(bot, statgDb, pubg, user, userID, channelID, message, evt);
+  } catch (err) {
+    logger.error(err);
+  }
 });
 
-bot.on('error', error => {
-
-    logger.info('bot.on("error")')
-    logger.error(error);
+bot.on('error', (error) => {
+  logger.info('bot.on("error")');
+  logger.error(error);
 });
 
-bot.on('disconnect', event => {
-
-    logger.info('bot.on("disconnect")')
-    logger.warn(event);
+bot.on('disconnect', (event) => {
+  logger.info('bot.on("disconnect")');
+  logger.warn(event);
 });
