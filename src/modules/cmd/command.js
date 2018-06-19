49835e127ffe08d18892baef07b4b1315bf5285f
DESKTOP-BVISHD1\Adrian <adrian.kuper@hotmail.de>
2018-06-19 15:31:39 +0200

    removed linting errors from command.js

--- src/modules/cmd/command.js
+++ src/modules/cmd/command.js
@@ -1,19 +1,15 @@
-const DiscordUser = require('./discord-user').DiscordUser
-
-
 class Command {
-    
-    /**
-     * 
-     * @param {string} cmd Command as string
-     * @param {string[]} args Array of string arguments
-     * @param {DiscordUser} discordUser discord user that issued the command
-     */
-    constructor (cmd, args, discordUser) {
-        this.command = cmd;
-        this.arguments = args;
-        this.discordUser = discordUser;
-    }
+  /**
+   *
+   * @param {string} cmd Command as string
+   * @param {string[]} args Array of string arguments
+   * @param {DiscordUser} discordUser discord user that issued the command
+   */
+  constructor(cmd, args, discordUser) {
+    this.command = cmd;
+    this.arguments = args;
+    this.discordUser = discordUser;
+  }
 }
 
 exports.Command = Command;
