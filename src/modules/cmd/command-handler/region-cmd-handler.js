dfecfac3fcfcb6f5f46841172a402a4e69da91c7
Adrian Kuper <adrian.kuper@hotmail.de>
2018-10-07 22:10:09 +0200

    i18n for region command

--- src/modules/cmd/command-handler/region-cmd-handler.js
+++ src/modules/cmd/command-handler/region-cmd-handler.js
@@ -1,6 +1,10 @@
 const { CommandHandler } = require('./cmd-handler.js');
 const regionHelper = require('../region-helper');
 
+const i18nCmdHandler = require('../../../i18n').getScope('commandHandler');
+const i18nRegion = require('../../../i18n').getScope('region');
+
+
 /**
  * Command handler for !statg region command. Sets global region
  * used by !statg register command.
@@ -14,13 +18,13 @@ class RegionCommandHandler extends CommandHandler {
       [newRegion] = cmd.arguments;
 
       if (!regionHelper.REGIONS.includes(newRegion)) {
-        this.onError(bot, cmd, new Error(`unknown region "${newRegion}"`));
+        this.onError(bot, cmd, new Error(i18nRegion.t('unknownRegion', { region: newRegion })));
         return Promise.resolve();
       }
 
       return db.setGlobalRegion(newRegion)
         .then(() => {
-          const message = `global region successfully set to "${newRegion}"!`;
+          const message = i18nRegion.t('successMessage', { region: newRegion });
           this.onSuccess(bot, cmd, message);
           return Promise.resolve();
         })
@@ -29,7 +33,7 @@ class RegionCommandHandler extends CommandHandler {
           return Promise.resolve();
         });
     }
-    this.onError(bot, cmd, new Error('invalid amount of arguments'));
+    this.onError(bot, cmd, new Error(i18nCmdHandler.t('invalidArguments')));
     return Promise.resolve();
   }
 }
