4786fb3e6a7e77fd2a77105af201b356879c2511
Adrian Kuper <adrian.kuper@hotmail.de>
2018-10-07 22:09:48 +0200

    i18n for help command

--- src/modules/cmd/command-handler/help-cmd-handler.js
+++ src/modules/cmd/command-handler/help-cmd-handler.js
@@ -1,4 +1,6 @@
 const { CommandHandler } = require('./cmd-handler.js');
+const i18nCmdHandler = require('../../../i18n').getScope('commandHandler');
+const i18nHelp = require('../../../i18n').getScope('help');
 
 const HELP_LINK = 'https://github.com/kuper-adrian/statg-bot/blob/master/README.md#Commands';
 
@@ -9,11 +11,11 @@ const HELP_LINK = 'https://github.com/kuper-adrian/statg-bot/blob/master/README.
 class HelpCommandHandler extends CommandHandler {
   handle(cmd, bot) {
     if (cmd.arguments.length !== 0) {
-      this.onError(bot, cmd, new Error('invalid amount of arguments'));
+      this.onError(bot, cmd, new Error(i18nCmdHandler.t('invalidArguments')));
       return;
     }
 
-    this.onSuccess(bot, cmd, `Click [here](${HELP_LINK}) to get help about all statg commands.`);
+    this.onSuccess(bot, cmd, i18nHelp.t('helpMessage', { HELP_LINK }));
   }
 }
 
