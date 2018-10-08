24b3f3baa6c1688076330ed34ff12d596a7e7a10
Adrian Kuper <adrian.kuper@hotmail.de>
2018-10-08 20:03:04 +0200

    i18n for status command

--- src/modules/cmd/command-handler/status-cmd-handler.js
+++ src/modules/cmd/command-handler/status-cmd-handler.js
@@ -1,5 +1,8 @@
 const { CommandHandler } = require('./cmd-handler.js');
 
+const i18nCmdHandler = require('../../../i18n').getScope('commandHandler');
+const i18nStatus = require('../../../i18n').getScope('status');
+
 /**
  * Command handler for !statg status command. Posts status of PUBG API.
  * @extends CommandHandler
@@ -7,13 +10,13 @@ const { CommandHandler } = require('./cmd-handler.js');
 class StatusCommandHandler extends CommandHandler {
   handle(cmd, bot, db, pubg) {
     if (cmd.arguments.length !== 0) {
-      this.onError(bot, cmd, new Error('invalid amount of arguments'));
+      this.onError(bot, cmd, new Error(i18nCmdHandler.t('invalidArguments')));
       return undefined;
     }
 
     return pubg.status()
       .then(() => {
-        this.onSuccess(bot, cmd, 'Looks like the PUBG API is up and ready!');
+        this.onSuccess(bot, cmd, i18nStatus.t('successMessage'));
       })
 
       .catch((error) => {
